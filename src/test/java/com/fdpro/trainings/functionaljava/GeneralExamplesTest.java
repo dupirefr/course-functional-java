11654c7e9dfa50829a4f57f182b414f4b3290cc2
fdpro <dupire.francois+pro@gmail.com>
2019-06-26 18:52:26 +0200

    Readded missing implementations

--- src/test/java/com/fdpro/trainings/functionaljava/GeneralExamplesTest.java
+++ src/test/java/com/fdpro/trainings/functionaljava/GeneralExamplesTest.java
@@ -10,28 +10,38 @@ class GeneralExamplesTest {
 
     @Test
     void transform_toUpperCase() {
-        Function<String, String> toUpperCase = null;
+        Function<String, String> toUpperCase = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.toUpperCase();
+            }
+        };
 
         assertThat(toUpperCase.apply("Input")).isEqualTo("INPUT");
     }
 
     @Test
     void transform_toLowerCase() {
-        Function<String, String> toLowerCase = null;
+        Function<String, String> toLowerCase = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.toLowerCase();
+            }
+        };
 
         assertThat(toLowerCase.apply("Input")).isEqualTo("input");
     }
 
     @Test
     void transform_doubleString_lambda() {
-        Function<String, String> doubleString = null;
+        Function<String, String> doubleString = s -> s + s;
 
         assertThat(doubleString.apply("Input")).isEqualTo("InputInput");
     }
 
     @Test
     void transform_trim_methodReference() {
-        Function<String, String> trim = null;
+        Function<String, String> trim = String::trim;
 
         assertThat(trim.apply("  Input  ")).isEqualTo("Input");
     }
